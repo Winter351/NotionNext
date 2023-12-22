0eb8b40daaee2d05ea50703808b2638e0eae4040
Winter351 <85336411+Winter351@users.noreply.github.com>
2023-12-22 02:45:21 +0000

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '欢迎来到我的博客', url: 'https://blog.wscn.vip' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'Linux相关知识点',
+  HEO_HERO_TITLE_2: 'V2Ray/XRay相关知识',
+  HEO_HERO_TITLE_3: 'VPS云服务',
+  HEO_HERO_TITLE_4: '青龙面板',
+  HEO_HERO_TITLE_5: '留言板',
+  HEO_HERO_TITLE_LINK: 'https://blog.wscn.vip',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -37,7 +37,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/Winter351', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
